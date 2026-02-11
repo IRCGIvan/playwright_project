a403602149609d183e17bebc3999e8647b643205
IRCGIvan <ivan.caraza@testiando.co>
2026-02-11 11:55:01 -0300

    Update index.js

--- performance-dashboard/src/router/index.js
+++ performance-dashboard/src/router/index.js
@@ -14,12 +14,3 @@ const router = createRouter({
 })
 
 export default router
-
-// import { createRouter, createWebHistory } from 'vue-router'
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [],
-// })
-
-// export default router
